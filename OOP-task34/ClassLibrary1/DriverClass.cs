254fd3c595890db558fc696b42f3e997609694e2
FrankCiceri <frankanthonyc00@gmail.com>
2023-04-20 11:13:05 -0500

    Finished methods

---
+++ OOP-task34/ClassLibrary1/DriverClass.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPtask34
+{
+    internal class DriverClass
+    {
+
+        public static void Main(string[] args) {
+
+
+            while (true) {
+
+                Console.WriteLine("Enter a number, press 0 to exit");
+                int number = Convert.ToInt32(Console.ReadLine());
+                if (number == 0) break;
+                Fibonacci.displayFibonacci(number);
+                Console.WriteLine();
+                Fibonacci.displayFibonacciRecursive(number);
+                Console.WriteLine();
+
+
+
+            }
+
+
+        }
+    }
+}
---
+++ OOP-task34/ClassLibrary1/Fibonacci.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPtask34
+{
+    public class Fibonacci{
+
+      
+
+
+        public static void displayFibonacci(int number) {
+            int fn2 = 1;
+            int fn1 = 0;            
+            int fn = 0;
+            Console.Write(fn1 + " ");
+            Console.Write(fn2 + " ");
+            while (fn < number) 
+            {
+                fn = fn1 + fn2;
+
+                fn1 = fn2;
+                fn2 = fn;
+                if (fn <= number) {
+                    Console.Write(fn + " ");
+                }
+               
+
+            }       
+
+
+        }
+
+        public static void displayFibonacciRecursive(int number, int fn1=0,int fn2=1)
+        {
+
+           if (fn1 == 0) {
+                Console.Write($"{fn1} {fn2} ");  
+            }
+            int fn = fn1 + fn2;
+            fn1 = fn2;
+            fn2 = fn;
+            if (fn <= number) {
+                Console.Write(fn + " ");
+                displayFibonacciRecursive(number, fn1, fn2);
+            }
+        }
+                
+    }
+}
